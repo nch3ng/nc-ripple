7c3579968f87a88206759f5c915895190462610a
Nate Cheng <boo0330@gmail.com>
2015-12-03 19:21:40 -0700

    Add drop duration

--- nc-ripple.js
+++ nc-ripple.js
@@ -3,25 +3,31 @@ angular.module('nc.ripple', [])
   return {
     restrict: 'A',
     scope: {
-      rTransparency: '@',
-      rSize: '@'
+      rOpacity: '@',
+      rSize: '@',
+      rDuration: '@'
     },
     link: function(scope, element, attrs) {
       element.css('overflow', 'hidden');
       element.css('transform', 'translateZ(0)');
       
-      if(scope.rTransparency && scope.rTransparency >= 0 && scope.rTransparency <=1)
+      if(scope.rOpacity && scope.rOpacity >= 0 && scope.rOpacity <=1)
         ;  
       else
-         scope.rTransparency = '.5';
+         scope.rOpacity = '.5';
       
       if(!scope.rSize)
         scope.rSize = 200;
+      if(!scope.rDuration)
+        scope.rDuration=1.5;
+      
+      scope.rDurationS = scope.rDuration +'s';
       
       var x, y=0, size={},
       offsets,
       func = function(e) {
-
+        
+        console.log(scope.rDurationS);
 
         var ripple = this.querySelector('b.drop');
 
@@ -33,18 +39,22 @@ angular.module('nc.ripple', [])
           // Prepend ripple to element
           this.insertBefore(ripple, this.firstChild);
           
-          ripple.style.background = 'rgba(220,220,220,' + scope.rTransparency + ')'
+          ripple.style.background = 'rgba(220,220,220,' + scope.rOpacity + ')'
           ripple.style.height=scope.rSize+'px';
           ripple.style.width=scope.rSize+'px';
-          
           size.x = ripple.offsetWidth;
           size.y = ripple.offsetHeight;
-          
+          //ripple.style.webkitTransitionDuration="1s";
         }
 
         var eventType = e.type;
 
         // Remove animation effect
+        ripple.style.animationDuration=null;
+        ripple.style.oAnimationDuration=null;
+        ripple.style.webkitAnimationDuration=null;
+        ripple.style.mozAnimationDuration=null;
+        ripple.style.msAnimationDuration=null;
         ripple.className = ripple.className.replace(/ ?(animate)/g, '');
 
         // get click coordinates by event type
@@ -69,11 +79,17 @@ angular.module('nc.ripple', [])
         innerPos.x = e.pageX;
         innerPos.y = e.pageY;
         
+        console.log()
 
         ripple.style.left = (innerPos.x - offsets.left - size.x / 2) + 'px';
         ripple.style.top = (innerPos.y - offsets.top - size.y / 2) + 'px';
 
         // Add animation effect
+        ripple.style.animationDuration=scope.rDurationS;
+        ripple.style.oAnimationDuration=scope.rDurationS;
+        ripple.style.webkitAnimationDuration=scope.rDurationS;
+        ripple.style.mozAnimationDuration=scope.rDurationS;
+        ripple.style.msAnimationDuration=scope.rDurationS;
         ripple.className += ' animate';
       }
       element.on("mouseup", func);
